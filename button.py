297f96ae5953f19bb2512d3e1b6de649d9b5c991
Spoorthi <saispoorthimunnangi2005@gmail.com>
2024-04-17 23:11:33 +0530

    done

---
+++ button.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Button:
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.x, self.y = pos
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.text_rect = self.text.get_rect(center=(self.x, self.y))
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def changeColor(self, position):
+        if self.rect.collidepoint(position):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
+    def checkForInput(self, position):
+        if position and self.rect.collidepoint(position):
+            return True
+        return False
